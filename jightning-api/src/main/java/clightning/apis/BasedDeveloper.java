1874a522e9ac517af22940237cf27abe81d48c2d
jayce <907861361@qq.com>
2020-04-05 14:00:23 +0800

    doc --- add doc for api classified as BasedDeveloper

--- jightning-api/src/main/java/clightning/apis/BasedDeveloper.java
+++ jightning-api/src/main/java/clightning/apis/BasedDeveloper.java
@@ -4,22 +4,25 @@ import clightning.apis.annotations.ParamTag;
 import clightning.apis.response.DevAddress;
 import clightning.apis.response.DevRescanOutput;
 
-import java.io.IOException;
-
 public interface BasedDeveloper {
+    /**
+     * Show addresses list up to derivation {index} (default is the last bip32 index)
+     *
+     * @return an arrary of {@code DevAddress}
+     */
     DevAddress[] devListAddrs();
 
     /**
-     * dev-listaddrs [bip32_max_index]
      * Show addresses list up to derivation {index} (default is the last bip32 index)
      *
-     * @return
+     * @return an arrary of {@code DevAddress}
      */
     DevAddress[] devListAddrs(@ParamTag(optional = true) int bip32MaxIndex);
 
     /**
-     * dev-rescan-outputs
      * Synchronize the state of our funds with bitcoind
+     *
+     * @return an array of {@code DevRescanOutput}
      */
     DevRescanOutput[] devRescanOutputs();
 }
